6f490a00baa834dc533a254a6633a047fcff8827
Max Reynolds <max@tillysinteriors.com>
2020-05-26 17:30:53 +0100

    Create PlayingBar.vue

---
+++ src/components/PlayingBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="fixed bottom-0 left-0 right-0 p-2" style="background-color: #f1f3f4;">
+    <p class="text-sm text-gray-800">
+      {{ song.title }}
+    </p>
+    <p class="text-xs text-gray-600">
+      {{ song.album }}
+    </p>
+    <p class="text-xs text-gray-600">
+      {{ song.creator }}
+    </p>
+    <audio controls autoplay :src="song.location" class="w-full" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayingBar',
+  props: {
+    song: {
+      type: Object,
+      required: false,
+      default () {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
