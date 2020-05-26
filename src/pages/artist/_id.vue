d997d4e55c53a5b87aea939341d5c49872ad04dc
Max Reynolds <max@tillysinteriors.com>
2020-05-26 16:11:20 +0100

    Update _id.vue

--- src/pages/artist/_id.vue
+++ src/pages/artist/_id.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    {{ $route.params.id }}
+    Artist {{ $route.params.id }}
+    <div v-if="errorMessage">
+      {{ errorMessage }}
+    </div>
+    <div v-else>
+      <pre>{{ result }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ArtistPage',
+  name: 'AlbumPage',
+  data () {
+    return {
+      result: null,
+      errorMessage: null,
+    }
+  },
+  async mounted () {
+    try {
+      this.result = await this.$store.dispatch('getTopTracksArtist', {
+        id: this.$route.params.id,
+        type: 'artist',
+      })
+    } catch (err) {
+      console.error(err)
+      this.errorMessage = err.message
+    }
+  },
 }
 </script>
