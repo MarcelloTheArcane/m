326b484eb354753c5b3c09563b033ac284da9abf
Max Reynolds <max@tillysinteriors.com>
2020-05-26 16:10:34 +0100

    Update _id.vue

--- src/pages/album/_id.vue
+++ src/pages/album/_id.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    Album {{ $route.params.id }}
+    <div v-if="errorMessage">
+      {{ errorMessage }}
+    </div>
+    <div v-else>
+      <img :src="albumData.image" class="h-12 w-12">
+      <h1>
+        {{ albumData.album }}
+      </h1>
+      <h2>
+        {{ albumData.creator }}
+      </h2>
+      <p>
+        {{ albumData.annotation }}
+      </p>
+
+      <pre>{{ album }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AlbumPage',
+  data () {
+    return {
+      album: [],
+      errorMessage: null,
+    }
+  },
+  async mounted () {
+    try {
+      this.album = await this.$store.dispatch('getAlbum', {
+        id: this.$route.params.id,
+      })
+    } catch (err) {
+      console.error(err)
+      this.errorMessage = err.message
+    }
+  },
+  computed: {
+    albumData () {
+      return this.album.length
+        ? this.album[0]
+        : {}
+    },
+  },
 }
 </script>
