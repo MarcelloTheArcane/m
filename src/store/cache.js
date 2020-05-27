97ba0d0476df7a71d6e62944952ffcc4533608ce
Max Reynolds <max@tillysinteriors.com>
2020-05-27 10:44:57 +0100

    Add cache

---
+++ src/store/cache.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const baseURL = 'REDACTED'
+
+const proxy = axios.create({
+  baseURL,
+  params: {
+    format: 'json',
+    num_tracks: 25,
+  },
+  headers: {
+    accept: 'application/json',
+  },
+})
+
+export default {
+  namespaced: true,
+  state () {
+    return {}
+  },
+  mutations: {
+    addToCache (state, [ key, value ]) {
+      state[key] = value
+    },
+  },
+  actions: {
+    async get ({ state, commit }, { path, params }) {
+      const cachePath = btoa(path + JSON.stringify(params))
+      if (!state[cachePath]) {
+        const { data } = await proxy.get(path, { params })
+        commit('addToCache', [ cachePath, data ])
+      }
+
+      return state[cachePath]
+    },
+  },
+}
