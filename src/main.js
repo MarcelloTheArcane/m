307e57b3c5cedad2fcd9b6391a4a6a7ebc24bceb
Max Reynolds <max@tillysinteriors.com>
2020-05-27 08:46:37 +0100

    Add vue lazyload

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueLazyload from 'vue-lazyload'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload)
 
 new Vue({
   router,
