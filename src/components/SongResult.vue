59b0d8e1e3488c7c4df4fb5ad097bbc4a2373936
Max Reynolds <max@tillysinteriors.com>
2020-05-27 08:46:52 +0100

    Add emit

--- src/components/SongResult.vue
+++ src/components/SongResult.vue
@@ -25,12 +25,14 @@
         Play next
       </button>
       <router-link
+        @click.native="$emit('select-option')"
         :to="`/artist/${songData.artistId[0]}`" 
         class="flex-1 border-gray-500 border text-center mx-2 py-2 bg-gray-100"
       >
         Artist
       </router-link>
       <router-link
+        @click.native="$emit('select-option')"
         :to="`/album/${songData.albumId}`" 
         class="flex-1 border-gray-500 border text-center mx-2 py-2 bg-gray-100"
       >
@@ -65,9 +67,11 @@ export default {
       this.expanded = !this.expanded
     },
     play () {
+      this.$emit('select-option')
       this.$store.commit('setPlayNow', [this.result])
     },
     playNext () {
+      this.$emit('select-option')
       this.$store.commit('setPlayNext', [this.result])
     },
   },
