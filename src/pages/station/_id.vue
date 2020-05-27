92f2fb22db424df980f23bc4a84266b925d28884
Max Reynolds <max@tillysinteriors.com>
2020-05-27 10:45:37 +0100

    Add station page

---
+++ src/pages/station/_id.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="bg-gray-200">
+    <div v-if="errorMessage">
+      {{ errorMessage }}
+    </div>
+    <div v-else class="flex flex-col items-center">
+      <div class="w-full bg-white rounded-t-lg p-3 mt-5">
+        <button @click="playAll" class="float-right focus:outline-none">
+          <svg viewBox="0 0 24 24" width="32" height="32" stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="12" cy="12" r="10" fill="#e2e8f0"></circle>
+            <polygon points="10 8 16 12 10 16 10 8"  stroke="currentColor" stroke-width="2"></polygon>
+          </svg>
+        </button>
+      </div>
+
+      <div class="w-full bg-white">
+        <song-result
+          v-for="(result, index) in results"
+          :key="index"
+          :result="result"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SongResult from '@/components/SongResult.vue'
+
+export default {
+  name: 'StationPage',
+  components: {
+    SongResult,
+  },
+  data () {
+    return {
+      results: null,
+      errorMessage: null,
+    }
+  },
+  async mounted () {
+    try {
+      this.results = await this.$store.dispatch('getListenNowList', {
+        station: this.$route.params.id,
+      })
+    } catch (err) {
+      console.error(err)
+      this.errorMessage = err.message
+    }
+  },
+  methods: {
+    playAll () {
+      this.$store.commit('setPlaylist', this.album)
+    },
+  },
+}
+</script>
