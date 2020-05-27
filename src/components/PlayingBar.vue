e7ab5f23dcfeeec247208ffce7e1720b88d785d0
Max Reynolds <max@tillysinteriors.com>
2020-05-27 08:46:02 +0100

    Add image to playing bar

--- src/components/PlayingBar.vue
+++ src/components/PlayingBar.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="fixed bottom-0 left-0 right-0 p-2" style="background-color: #f1f3f4;">
-    <p class="text-sm text-gray-800">
-      {{ song.title }}
-    </p>
-    <p class="text-xs text-gray-600">
-      {{ song.album }}
-    </p>
-    <p class="text-xs text-gray-600">
-      {{ song.creator }}
-    </p>
-    <audio controls autoplay :src="song.location" class="w-full" />
+  <div class="p-2" style="background-color: #f1f3f4;">
+    <div class="flex flex-row" v-if="song.location">
+      <img :src="song.image" class="w-12 h-12 my-1">
+      <div class="mx-2 flex-1 min-w-0">
+        <p class="text-sm text-gray-800 truncate w-full">
+          {{ song.title }}
+        </p>
+        <p class="text-xs text-gray-600 truncate w-full">
+          {{ song.album }}
+        </p>
+        <p class="text-xs text-gray-600 truncate w-full">
+          {{ song.creator }}
+        </p>
+      </div>
+    </div>
+    <audio controls autoplay :src="song.location" class="w-full focus:outline-none" />
   </div>
 </template>
 
