d4003528f44e365d680b249fbd09234e6c2284d1
Max Reynolds <max@tillysinteriors.com>
2020-06-11 07:08:31 +0100

    Fix desktop sizing #18

--- src/components/PlayingBar.vue
+++ src/components/PlayingBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex md:flex-row flex-col colour-theme">
+  <div class="flex flex-col colour-theme">
     <progress
       min="0"
       max="1"
@@ -8,7 +8,7 @@
       ref="progress"
     />
 
-    <div class="flex flex-row justify-between mx-2 my-1 text-sm">
+    <div class="flex flex-row justify-between px-2 py-1 text-sm w-full">
       <p>
         {{ songTime | minutes }}
       </p>
@@ -17,86 +17,89 @@
       </p>
     </div>
 
-    <div class="mx-2 flex flex-row lg:flex-1 lg:pl-6 md:pl-3">
-      <img v-lazy="song.image" class="w-12 h-12 my-1">
-      <div class="mx-2 flex-1 min-w-0">
-        <p class="text-sm truncate w-full">
-          {{ song.title }}
-        </p>
-        <p class="text-xs truncate w-full">
-          {{ song.album }}
-        </p>
-        <p class="text-xs truncate w-full">
-          {{ song.creator }}
-        </p>
+    <div class="flex md:flex-row flex-col">
+
+      <div class="mx-2 flex flex-row md:flex-1 pl-0">
+        <img v-lazy="song.image" class="w-12 h-12 my-1">
+        <div class="mx-2 flex-1 min-w-0">
+          <p class="text-sm truncate w-full">
+            {{ song.title }}
+          </p>
+          <p class="text-xs truncate w-full">
+            {{ song.album }}
+          </p>
+          <p class="text-xs truncate w-full">
+            {{ song.creator }}
+          </p>
+        </div>
       </div>
-    </div>
 
-    <div class="w-full flex-1 flex flex-row items-center pb-2 px-2">
-      <button
-        @click="playPreviousSong"
-        class="p-2 focus:outline-none"
-        :style="`opacity: ${playDisabled ? 0.6 : 1}`"
-      >
-        <svg viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
-          <polygon points="11 19 2 12 11 5 11 19"></polygon>
-          <polygon points="22 19 13 12 22 5 22 19"></polygon>
-        </svg>
-      </button>
+      <div class="md:w-auto w-full flex flex-row items-center pb-2 px-2">
+        <button
+          @click="playPreviousSong"
+          class="p-2 focus:outline-none"
+          :style="`opacity: ${playDisabled ? 0.6 : 1}`"
+        >
+          <svg viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
+            <polygon points="11 19 2 12 11 5 11 19"></polygon>
+            <polygon points="22 19 13 12 22 5 22 19"></polygon>
+          </svg>
+        </button>
 
-      <button
-        @click="togglePlaying"
-        class="p-2 focus:outline-none"
-        :style="`opacity: ${playDisabled ? 0.6 : 1}`"
-      >
-        <svg v-if="paused" viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
-          <polygon points="5 3 19 12 5 21 5 3"></polygon>
-        </svg>
-        <svg v-else viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
-          <rect x="6" y="4" width="4" height="16"></rect>
-          <rect x="14" y="4" width="4" height="16"></rect>
-        </svg>
-      </button>
+        <button
+          @click="togglePlaying"
+          class="p-2 focus:outline-none"
+          :style="`opacity: ${playDisabled ? 0.6 : 1}`"
+        >
+          <svg v-if="paused" viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
+            <polygon points="5 3 19 12 5 21 5 3"></polygon>
+          </svg>
+          <svg v-else viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
+            <rect x="6" y="4" width="4" height="16"></rect>
+            <rect x="14" y="4" width="4" height="16"></rect>
+          </svg>
+        </button>
 
-      <button
-        @click="playNextSong"
-        class="p-2 focus:outline-none"
-        :style="`opacity: ${playDisabled ? 0.6 : 1}`"
-      >
-        <svg viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
-          <polygon points="13 19 22 12 13 5 13 19"></polygon>
-          <polygon points="2 19 11 12 2 5 2 19"></polygon>
-        </svg>
-      </button>
+        <button
+          @click="playNextSong"
+          class="p-2 focus:outline-none"
+          :style="`opacity: ${playDisabled ? 0.6 : 1}`"
+        >
+          <svg viewBox="0 0 24 24" width="38" height="38" fill="currentColor">
+            <polygon points="13 19 22 12 13 5 13 19"></polygon>
+            <polygon points="2 19 11 12 2 5 2 19"></polygon>
+          </svg>
+        </button>
 
-      <span class="flex-1">&nbsp;</span>
+        <span class="flex-1">&nbsp;</span>
 
-      <button
-        v-if="$store.getters['favourites/locations'].includes(song.location)"
-        @click="$store.dispatch('favourites/delete', song)"
-        class="p-2 focus:outline-none"
-      >
-        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="currentColor" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-        </svg>
-      </button>
-      <button
-        v-else
-        @click="$store.dispatch('favourites/add', song)"
-        class="p-2 focus:outline-none"
-      >
-        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-        </svg>
-      </button>
+        <button
+          v-if="$store.getters['favourites/locations'].includes(song.location)"
+          @click="$store.dispatch('favourites/delete', song)"
+          class="p-2 focus:outline-none"
+        >
+          <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="currentColor" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+          </svg>
+        </button>
+        <button
+          v-else
+          @click="$store.dispatch('favourites/add', song)"
+          class="p-2 focus:outline-none"
+        >
+          <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+          </svg>
+        </button>
 
-      <a :href="song.location" class="p-2 focus:outline-none" :download="song.title">
-        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <polyline points="8 17 12 21 16 17"></polyline>
-          <line x1="12" y1="12" x2="12" y2="21"></line>
-          <path d="M20.88 18.09A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.29"></path>
-        </svg>
-      </a>
+        <a :href="song.location" class="p-2 focus:outline-none" :download="song.title">
+          <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <polyline points="8 17 12 21 16 17"></polyline>
+            <line x1="12" y1="12" x2="12" y2="21"></line>
+            <path d="M20.88 18.09A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.29"></path>
+          </svg>
+        </a>
+      </div>
     </div>
 
     <div class="w-full flex-1" ref="audio">
