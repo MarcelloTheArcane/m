056971732ccf250920a4d2e67c9742e1cccfb719
Max Reynolds <max@tillysinteriors.com>
2020-06-30 10:54:06 +0100

    Fixed margin

--- src/components/ButtonList.vue
+++ src/components/ButtonList.vue
@@ -17,7 +17,7 @@
     </button>
     <button
       @click="$emit('play-all')"
-      class="border-gray-500 border text-center text-sm mx-2 py-2 px-3 bg-gray-200 text-gray-800"
+      class="border-gray-500 border text-center text-sm ml-2 py-2 px-3 bg-gray-200 text-gray-800"
       title="Play album"
     >
       Play
